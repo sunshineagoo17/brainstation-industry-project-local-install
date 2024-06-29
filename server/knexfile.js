cc7354f7138c71953b24d6c98826ce68adcee8fa
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-06-28 23:23:08 -0400

    Configured JawsDB Maria connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const url = require('url');
+
+// Parse the JAWSDB_MARIA_URL
+const dbUrl = process.env.JAWSDB_MARIA_URL ? url.parse(process.env.JAWSDB_MARIA_URL) : null;
+const [user, password] = dbUrl ? dbUrl.auth.split(':') : [null, null];
+const dbName = dbUrl ? dbUrl.path.substring(1) : null;
+const dbHost = dbUrl ? dbUrl.hostname : null;
+const dbPort = dbUrl ? dbUrl.port : null;
 
 module.exports = {
   development: {
@@ -21,10 +29,11 @@ module.exports = {
   production: {
     client: 'mysql',
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: dbHost,
+      user: user,
+      password: password,
+      database: dbName,
+      port: dbPort,
       charset: 'utf8'
     },
     migrations: {
