0d8307ba6d3b329031d1070790d8c71915317e86
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-06-28 22:37:59 -0400

    Added test connection

--- server/index.js
+++ server/index.js
@@ -2,14 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const csvtojson = require('csvtojson');
-let cors;
-try {
-  cors = require('cors');
-  console.log('CORS module loaded successfully');
-} catch (error) {
-  console.error('Failed to load CORS module:', error);
-}
-require('dotenv').config();
+const cors = require('cors');
+require('dotenv').config(); // Load environment variables at the very top
 
 const app = express();
 const router = express.Router();
@@ -22,17 +16,24 @@ const retailersRoutes = require('./routes/retailers');
 const dataRoutes = require('./routes/data');
 
 // Load environment variables
-const { CORS_ORIGIN, DATA_DIR } = process.env;
+const { CORS_ORIGIN, DATA_DIR, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, NODE_ENV } = process.env;
+
+// Log the loaded environment variables to ensure they're correct
+console.log('Loaded environment variables:', {
+  CORS_ORIGIN,
+  DATA_DIR,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  NODE_ENV
+});
 
 // Enable CORS for all routes if the module is loaded
-if (cors) {
-  app.use(cors({
-    origin: CORS_ORIGIN,
-    credentials: true
-  }));
-} else {
-  console.error('CORS middleware not loaded.');
-}
+app.use(cors({
+  origin: CORS_ORIGIN,
+  credentials: true
+}));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -57,6 +58,26 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+// Endpoint to test database connection
+app.get('/test-db-connection', async (req, res) => {
+  try {
+    const knex = require('knex')({
+      client: 'mysql',
+      connection: {
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME
+      }
+    });
+    await knex.raw('SELECT 1+1 AS result');
+    res.status(200).json({ message: 'Database connection successful' });
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    res.status(500).json({ message: 'Database connection failed', error });
+  }
 });
 
 // Platform-specific logging
