24d976dde355eb2c206ffc9eedc334193c564cf9
sunshineagoo17 <sunshine.agoo@gmail.com>
2024-06-29 09:36:52 -0400

    Fixed apis

--- server/api/Api.js
+++ server/api/Api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || 'https://spectra-de1476b6df25.herokuapp.com',
+  baseURL: process.env.BASE_URL || 'https://dell-technologies-spectra-8b1af00a7312.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
   },
